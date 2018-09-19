b1350531b4481ca0064658f9793adb2301a83997
Bruno <mozbls7@gmail.com>
2018-09-19 19:52:15 -0300

    commint inicial

---
+++ js/form.js
@@ -0,0 +1,13 @@
+
+//$(document).ready(function(){
+$(function() {
+   
+   $('#btn-cadastrar').click(function(){
+     
+     var nome = $('input[name=nome]').val();
+       
+   });
+    
+});
+
+
